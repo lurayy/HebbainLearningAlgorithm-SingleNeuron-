3b10b65060f22b56183f1c69ac5c8edf031692b1
Kabir S. Tamari <rabbit.bonex@gmail.com>
2018-06-05 01:45:02 +0545

    Complete

--- src/app.py
+++ src/app.py
@@ -1,42 +1,46 @@
 import json
 
-
-class Perceptron:
+class perceptron:
 
     def __init__(self):
-        self.w1 = 0
-        self.w2 = 0
-        self.bais = 0
-        self.threshold = 0
+        self.ndata = {
+            'w1': 0,
+            'w2': 0,
+            'b' : 0,
+            'threshold' : 0
+        }
 
-    def Activation_fuction(self, x1, x2, b):
-        a =  self.w1 * x1 + self.w2 * x2 + b * self.bais
-        if a > self.threshold:
+    def n_fuction(self, x1 ,x2 ,b):
+        atf = self.ndata['w1'] * x1 + self.ndata['w2'] * x2 + b * self.ndata['b']
+        if atf > self.ndata['threshold']:
             return 1
-        else
+        else:
             return -1
 
-
-def Training():
-    # defining neuron
-    neuron1 = Perceptron()
-
-    # taking data from the training json file
+def training(neuron1):
     with open('trainindata.json') as d:
         t_data = json.load(d)
 
-    # using hebbian learning algorithm
-    i = 0
-    x2 = t_data["x2"]
-    bais = t_data["bais"]
-    Y = t_data["tarY"]
-
-    for x1 in t_data["x2"]:
-        neuron1.w1 = neuron1.w1 + x1 * Y[i]
-        neuron1.w2 = neuron1.w2 + x2[i] * Y[i]
-        neuron1.bais = neuron1.bais + Y[i]
+    i=0
+    x1 = t_data['x1']
+    x2 = t_data['x2']
+    b = t_data['bais']
+    for y in t_data['tarY']:
+        neuron1.ndata['w1'] = neuron1.ndata['w1'] + x1[i] * y
+        neuron1.ndata['w2'] = neuron1.ndata['w2'] + x2[i] * y
+        neuron1.ndata['b'] = neuron1.ndata['b'] + y
         i = i + 1
+
+def test(xt1 , xt2, tb):
+    return neuron1.n_fuction(xt1 , xt2 , tb)
+
 
 
 if __name__ == "__main__":
-    Training()
+    neuron1 = perceptron()
+    training(neuron1)
+    print("Enter input x1 and x2:")
+    ix1 = int(input())
+    ix2 = int(input())
+    tb = 1
+    print(test(ix1 , ix2, tb))
